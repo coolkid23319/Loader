962a90e85e4f44ff580d38d6012245bfc6010279
coolkid23319 <162951844+coolkid23319@users.noreply.github.com>
2024-08-12 18:06:23 -0400

    Update Revive_Loader.lua

--- Revive_Loader.lua
+++ Revive_Loader.lua
@@ -1,2 +1,2 @@
 script_key= getgenv().revive.Target.Normal.scriptKey
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/49cd44dfc283bd4bebf02b74b5e18efb.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9d10b10ed0b957da52a0ff21e82573d4.lua"))()
